ac0fb5ad3faabbec1989d85af9beb8356bf05693
Stas <stevinel@xaker.ru>
2021-06-22 16:43:23 +0300

    for review without docker

--- recipes/models.py
+++ recipes/models.py
@@ -53,6 +53,10 @@ class Tag(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
 
 class Favorite(models.Model):
     user = models.ForeignKey(
